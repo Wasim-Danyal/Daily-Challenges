88f7ddd84432f63c8b57895f78c3f38d2577ef13
Wasim-Danyal <wasim.danyal1@gmail.com>
2020-07-16 20:14:19 +0100

    Challenge One

---
+++ julydevops.py
@@ -0,0 +1,10 @@
+Names = [
+    "Christopher","John","Jason","Arsalan","Domenico",
+    "Joshua","Ryan","Bradley","Javas","Sithembiso","Jacob","Clifford",
+    "Wasim","Mohamed","Tobias","Christopher","Samuel","Edmund","Amanda",
+    "Steven","Diarmuid"
+    ]
+
+print('Updated List:', Names)
+print(Names[5])
+print(Names.count("Christopher"))
