2e2921938f3d357ee19be027690fb1309019bd2f
Wasim-Danyal <68053858+Wasim-Danyal@users.noreply.github.com>
2020-07-27 22:37:37 +0100

    Update test_SevenNotFive.py

--- COTD_27_07_2020/test/test_SevenNotFive.py
+++ COTD_27_07_2020/test/test_SevenNotFive.py
@@ -2,5 +2,5 @@ import pytest
 from program import usefrom
 
 def test_one():
-	if isinstance(usefrom.sevennotfive(2000,3200), int):
+	if isinstance(usefrom.sevennotfive(2000,3201), int):
 		return True
