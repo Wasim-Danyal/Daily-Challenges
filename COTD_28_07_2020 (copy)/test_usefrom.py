386177f066fde63d64e527f739e38c2ef892c7aa
Wasim-Danyal <68053858+Wasim-Danyal@users.noreply.github.com>
2020-07-28 22:01:25 +0100

    duplicated

--- COTD_28_07_2020 (copy)/test_usefrom.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from program import usefrom
-
-def test_one():
-    assert(usefrom.name("I am in Amsterdam am I?") == 2)
-    return True
-
-def test_two():
-    isinstance(usefrom.name("I am in Amsterdam am I?"), str)
